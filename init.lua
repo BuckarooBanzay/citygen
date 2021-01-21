121b74b841f433c787a3cb9abdd8bce509ca6874
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-01-21 19:54:58 +0100

    drop some more stuff

--- init.lua
+++ init.lua
@@ -5,7 +5,3 @@ local MP = minetest.get_modpath("citygen")
 
 -- common stuff
 dofile(MP.."/mapgen.lua")
-
-if minetest.settings:get_bool("enable_citygen_integration_test") then
-        dofile(MP.."/integration_test.lua")
-end

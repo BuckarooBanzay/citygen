2b6b0511be41894dbfb54dad239ae904a6909f8e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-09-30 15:28:47 +0200

    add sanity check

--- init.lua
+++ init.lua
@@ -3,6 +3,11 @@ citygen = {
 	cityblock_size = 20
 }
 
+-- sanity checks
+if minetest.get_mapgen_setting("mg_name") ~= "singlenode" then
+    error("this mod only works with the 'singlenode' mapgen")
+end
+
 local MP = minetest.get_modpath("citygen")
 
 dofile(MP.."/api.lua")
